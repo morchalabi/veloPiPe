e1660eef2e1a2c7685907ee8405ad24967c12401
Morteza <mor.chalabi@gmail.com>
2021-02-14 19:49:48 +0100

    adding files

---
+++ src/annotation_generator.R
@@ -0,0 +1,64 @@
+library(readr)
+library(parallel)
+
+# STEP 1: Setting up options ####
+
+inURL_ = 'ftp://ftp.ensembl.org/pub/release-102/gtf/mus_musculus/Mus_musculus.GRCm38.102.gtf.gz'      # URL+gtf file name
+biotype_ = c('protein_coding')                                                                        # type of exons to extract
+chrs_ = '^[1-9]+|^X|^Y|^MT'                                                                           # regular expression for extracting canonical and MT chromosomes
+cpu_ = 3                                                                                              # number of CPU cores
+
+# STEP 1: Reading in gtf file online ####
+message('Reading in gtf file online')
+
+mus_gtf = read_delim(file = inURL_, comment = '#', na = '.', delim = '\t',
+                     col_names = c('seq','source','feature','start','end','score','strand','frame','attribute'), col_types = 'cccdddcic')
+
+mus_gtf = mus_gtf[grepl(x = mus_gtf$seq, pattern = chrs_ , perl = T),c("seq","feature","start","end","attribute","strand")]     # extracting chromosomes/sequences
+mus_gtf = mus_gtf[order(mus_gtf$seq, mus_gtf$start, mus_gtf$end),]                                                              # sorting gtf by seq, start and end
+
+# STEP 2: Generating gene table ####
+message('Generating gene table')
+
+# not possible to filer out non-protein-coding genes here as some pseudo-genes have protein coding transcripts
+genes_tbl = as.data.frame(mus_gtf[mus_gtf$feature %in% 'gene',])
+genes_ = unlist(mclapply(X = genes_tbl$attribute, mc.cores = cpu_,  FUN =      # extracting all MGI genes
+                                                                    function(att_)
+                                                                    {
+                                                                      annots_ = strsplit(x = att_, split = '; ')[[1]]
+                                                                      annots_ = annots_[grepl(x = annots_, pattern = 'gene_name')]
+                                                                      gene_ = strsplit(x = annots_[1], split = '"')[[1]][2]
+                                                                      return(gene_)
+                                                                    }))
+inds_ = !duplicated(genes_, fromLast = T)     # keeps one copy of conflicting genes. Genes are sorted by start and then end; using fromLast = T make this command keep last observations having longer loci
+genes_ = genes_[inds_]
+genes_tbl = genes_tbl[inds_,c('seq','start','end')]
+rownames(genes_tbl) = genes_
+
+# STEP 3: Generating exon table ####
+message('Generating exon table')
+
+exons_tbl = mus_gtf[mus_gtf$feature %in% 'exon',]
+gene_exon = do.call(mclapply(X = exons_tbl$attribute, mc.cores = cpu_,  FUN =      # extracting MGI protein-coding exons
+                                                                        function(att_)
+                                                                        {
+                                                                          annots_ = strsplit(x = att_, split = '; ')[[1]]
+                                                                          annots_ = annots_[grepl(x = annots_, pattern = 'gene_name|transcript_biotype|exon_id')]
+                                                                          btyp_ = strsplit(x = annots_[2], split = '"')[[1]][2]
+                                                                          if(btyp_ %in% biotype_)
+                                                                          {
+                                                                            gene_ = strsplit(x = annots_[1], split = '"')[[1]][2]
+                                                                            exon_ = strsplit(x = annots_[3], split = '"')[[1]][2]
+                                                                            return(c(gene = gene_, exon = exon_))
+                                                                          }
+                                                                          return(NA)
+                                                                        }), what = rbind)
+
+exons_tbl = cbind(gene_exon, exons_tbl[,c("seq","start","end", "strand")])
+exons_tbl = exons_tbl[!is.na(exons_tbl$gene),]
+exons_tbl = exons_tbl[!duplicated(exons_tbl[,c("start","end")]), ]      # one exon can be part of several transcripts
+genes_tbl = genes_tbl[unique(exons_tbl$gene),]                          # some genes in genes_tbl are not protein coding
+
+# STEP 4: Saving data ####
+
+save(genes_tbl, exons_tbl, file = '../data/genes_exons_tables.RData')
---
+++ src/veloPiPe.R
@@ -0,0 +1,103 @@
+library(parallel)
+library(Matrix)
+
+# STEP 1: Setting up options ####
+
+cpu_ = 50      # number of CPU cores
+
+# STEP 2: Loading exon and gene tables ####
+
+load(file = '../data/genes_exons_tables.RData')
+
+# STEP 3: Processing batch BAM files
+
+btchs_ = list.files(path = '../data', pattern = 'batch_*', full.names = T)      # reading in batch folders
+for(btch_ in btchs_)
+{
+  message('<< Processing bam files in ',btch_, ' >>')
+  
+  bamfiles_ = list.files(path = btch_, pattern = '*.bam\\b')                                          # listing bam and bai files
+  bamfiles_ = bamfiles_[!grepl(x = bamfiles_, pattern = '*.bai\\b')]                                  # filtering our bai files
+  cells_ = sapply(X = strsplit(bamfiles_, split = '[.]'), FUN = function(elm_) return(elm_[[1]]))     # extracting cell names
+  bamfiles_ = paste0(btch_,'/',bamfiles_)
+  
+  rslt_btch = mclapply(X = bamfiles_, mc.cores = cpu_, genes_tbl, exons_tbl,  FUN =
+                                                                              function(bamfile_, genes_tbl, exons_tbl)
+                                                                              {
+                                                                                # step 3.1: Generating alignment table ####
+                                                                                
+                                                                                tmp_ = paste0(bamfile_,'.txt')                                                                                            # temp out file
+                                                                                system2(command = 'samtools', args = c('view', '-b', bamfile_, '|', 'bamToBed', '-i', 'stdin','>', tmp_))
+                                                                                aligns_ = read.table(file = tmp_, sep = '\t', header = F, col.names = c('seq','start','end','name','score','strand'))     # alignments are already position sorted
+                                                                                system2(command = 'rm', args = tmp_)
+                                                                                
+                                                                                # step 3.2: Finding spliced and unspliced alignments ####
+                                                                                
+                                                                                spliced_vec = unspliced_vec = integer(length = nrow(genes_tbl))     # vector containing spliced counts
+                                                                                names(spliced_vec) = names(unspliced_vec) = rownames(genes_tbl)     # vector containing unspliced counts
+                                                                                
+                                                                                for(seq_ in unique(aligns_$seq))                                    # for each sequence
+                                                                                {
+                                                                                  genes_tbl_sub = genes_tbl[genes_tbl$seq %in% seq_,]     # table of genes at chr seq_
+                                                                                  exons_tbl_sub = exons_tbl[exons_tbl$seq %in% seq_,]     # table of exons at chr seq_
+                                                                                  aligns_sub    = aligns_[    aligns_$seq %in% seq_,]     # table of alignments at chr seq_
+                                                                                  
+                                                                                  for(rw_ in 1:nrow(aligns_sub))                          # for each alignment
+                                                                                  {
+                                                                                    align_ = aligns_sub[rw_,]                                                                                       # locus of current alignment
+                                                                                    align_str_exons = exons_tbl_sub[ exons_tbl_sub$start <= align_$start & align_$start <= exons_tbl_sub$end, ]     # finding exons crossing start of alignment
+                                                                                    align_end_exons = exons_tbl_sub[ exons_tbl_sub$start <= align_$end   & align_$end   <= exons_tbl_sub$end, ]     # finding exons crossing end of alignment
+                                                                                    genes_ = union(align_str_exons$gene, align_end_exons$gene)                                                      # genes whose exons are crossed by either end of current alignment
+                                                                                    
+                                                                                    # << Case 1 >>: if neither end of current alignment crosses any exon (alignment is either in intronic or intergenic region)
+                                                                                    
+                                                                                    if(nrow(align_str_exons) == 0 & nrow(align_end_exons) == 0)
+                                                                                    {
+                                                                                      gene_ = rownames(genes_tbl_sub[ genes_tbl_sub$start <= align_$start & align_$end <= genes_tbl_sub$end, ])     # gene that contains current alignment
+                                                                                      if(length(gene_) == 1){ spliced_vec[gene_] = spliced_vec[gene_] + 1 }                                         # alignment is in intronic region of a gene
+                                                                                      next()
+                                                                                    }
+                                                                                    
+                                                                                    # << Case 2 >>: if both ends of current alignment cross any exon
+                                                                                    
+                                                                                    if(nrow(align_str_exons) != 0 & nrow(align_end_exons) != 0)
+                                                                                    {
+                                                                                      cmn_exons = intersect(x = align_str_exons$exon, y = align_end_exons$exon)     # common exons crossed by both ends of current alignment
+                                                                                      
+                                                                                      if(length(cmn_exons) != 0)                                                    # there is at least one exon crossed by both ends of current alignment
+                                                                                      {
+                                                                                        unsp_genes = unique(align_str_exons[align_str_exons$exon %in% cmn_exons,"gene"])      # only genes of common exons
+                                                                                        unspliced_vec[unsp_genes] = unspliced_vec[unsp_genes] + 1
+                                                                                        
+                                                                                      }else                                                                         # crossed exons are apart; they are from either one gene or different genes
+                                                                                      {
+                                                                                        spliced_vec[genes_] = spliced_vec[genes_] + 1
+                                                                                      }
+                                                                                      next()
+                                                                                    }
+                                                                                    
+                                                                                    # << Case 3 >>: only one end of current alignment crosses exon(s)
+                                                                                    
+                                                                                    spliced_vec[genes_] = spliced_vec[genes_] + 1
+                                                                                  }
+                                                                                }
+                                                                                
+                                                                                system2(command = 'echo', args = c('done for ', bamfile_))
+                                                                                return( cbind(unspliced = unspliced_vec, spliced = spliced_vec))
+                                                                              })
+  # STEP 4: Writing matrices to disk ####
+  
+  names(rslt_btch) = cells_
+  spliced_mat = unspliced_mat = Matrix(data = 0, nrow = nrow(genes_tbl), ncol = length(cells_), sparse = T,
+                                       dimnames = list(gene = rownames(genes_tbl), cell = cells_))
+  for(cell_ in cells_)
+  {
+    spliced_mat[,cell_]   = rslt_btch[[cell_]][,"spliced"]
+    unspliced_mat[,cell_] = rslt_btch[[cell_]][,"unspliced"]
+  }
+  null_ = writeMM(obj = spliced_mat,   file = paste0(btch_,'/','spliced.mtx'))      # sparse matrix for spliced counts
+  null_ = writeMM(obj = unspliced_mat, file = paste0(btch_,'/','unspliced.mtx'))
+  cat(rownames(genes_tbl), sep = '\n', file = paste0(btch_,'/','genes.tsv'))        # genes
+  cat(cells_, sep = '\n',              file = paste0(btch_,'/','barcodes.tsv'))     # cells
+}
+
